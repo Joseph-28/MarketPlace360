07e85b22db6d64948a53fc3bbed3325fabafbf5b
Joseph Gonzalez Herrera <goteks28@gmail.com>
2026-04-25 21:02:36 -0600

    Ajuste rutas de componentes

--- JS/componentes.js
+++ JS/componentes.js
@@ -1,7 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     //Cargar el header
-    fetch("/HTML/componentes/header.html")
+    fetch("HTML/componentes/header.html")
         .then(res => {
             if (!res.ok) throw new Error("Error al cargar header");
             return res.text();
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
         .catch(err => console.error(err));
 
     //Cargar el footer
-    fetch("/HTML//componentes/footer.html")
+    fetch("HTML/componentes/footer.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer").innerHTML = data;
